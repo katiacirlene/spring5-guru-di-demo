85dd52f3ef849f9d065565201b663767c77008f2
Katia <katiacirlene@bitbucket.org>
2018-05-17 13:30:45 -0300

    adding "spring5-guru-di-demo" project generated using Spring Initializr.

---
+++ src/main/java/com/mytests/spring5gurudidemo/Spring5GuruDiDemoApplication.java
@@ -0,0 +1,12 @@
+package com.mytests.spring5gurudidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5GuruDiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5GuruDiDemoApplication.class, args);
+	}
+}
