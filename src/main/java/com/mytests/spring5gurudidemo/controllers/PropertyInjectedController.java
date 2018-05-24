4a4f07f6f9f37dee287a2d9532959a16dc0fdfa2
Katia <katiacirlene@bitbucket.org>
2018-05-24 19:27:29 -0300

    using @Qualifier annotation.

--- src/main/java/com/mytests/spring5gurudidemo/controllers/PropertyInjectedController.java
+++ src/main/java/com/mytests/spring5gurudidemo/controllers/PropertyInjectedController.java
@@ -2,12 +2,14 @@ package com.mytests.spring5gurudidemo.controllers;
 
 import com.mytests.spring5gurudidemo.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
 
     @Autowired
+    @Qualifier("greetingServiceImpl")
     public GreetingService greetingService;
 
     public String sayHello() {
--- src/main/java/com/mytests/spring5gurudidemo/controllers/SetterInjectedController.java
+++ src/main/java/com/mytests/spring5gurudidemo/controllers/SetterInjectedController.java
@@ -2,6 +2,7 @@ package com.mytests.spring5gurudidemo.controllers;
 
 import com.mytests.spring5gurudidemo.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -14,6 +15,7 @@ public class SetterInjectedController {
     }
 
     @Autowired
+    @Qualifier("setterGreetingService")
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
